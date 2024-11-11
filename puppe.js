f4004bcb81d572290d214ab11a0cc12b8ae1f151
Vinícius Ormenesse <viniciusormenesse@gmail.com>
2024-11-11 20:49:41 -0300

    Performance Changes

--- puppe.js
+++ puppe.js
@@ -1,17 +1,12 @@
 const puppeteer = require("puppeteer");
+const pptr = require('puppeteer-core');
 process.env.TZ = 'Europe/Rome';
 function waitUntil(targetHour, targetMinute, targetSecond) {
     return new Promise((resolve) => {
         const interval = setInterval(() => {
             const now = new Date();
-            // console.log(now)
-            // console.log(
-            //     now.getHours(),
-            //     now.getMinutes(),
-            //     now.getSeconds()
-            // )
-            if (now.getHours() === targetHour && 
-                now.getMinutes() === targetMinute &&
+            if (now.getHours() >= targetHour && 
+                now.getMinutes() >= targetMinute &&
                 now.getSeconds() >= targetSecond
             ) {
                 clearInterval(interval);
@@ -29,13 +24,17 @@ async function check_if_available(page) {
     return !availability;
 }
 
-async function navigate(page, wait_for_rome=false) {
+async function navigate(page, wait_for_rome=false, login=true) {
     
     try {
-        await page.goto("https://prenotami.esteri.it/Home", {waitUntil: "networkidle2"});
-        await page.type("input[id=login-email]",process.env.email);
-        await page.type("input[id=login-password]",process.env.password);
-        await page.click('button[type="submit"]');
+        if (login) {
+            await page.goto("https://prenotami.esteri.it/Home", {waitUntil: "networkidle2"});
+            await page.type("input[id=login-email]",process.env.email);
+            await page.type("input[id=login-password]",process.env.password);
+            await page.click('button[type="submit"]');
+        } else {
+            await page.goto("https://prenotami.esteri.it/Home", {waitUntil: "networkidle2"});
+        }
         await page.waitForSelector('a[href="/Services"]');
         if (!check_if_available(page)) {
             throw new Error('Page unavailable');
@@ -52,15 +51,23 @@ async function navigate(page, wait_for_rome=false) {
         await page.waitForNavigation();
     } catch (error){
         console.log("Error occured:",error);
-        navigate(page, wait_for_rome);
+        navigate(page, wait_for_rome, false);
     }
 }
 
 (async () => {
-    await waitUntil(23,45,0);
-    const browser = await puppeteer.launch({headless: false, args: [`--window-size=1366,720`], defaultViewport: { width:1366, height: 720} });
+    // await waitUntil(23,45,0);
+    const browser = await pptr.launch(
+        {
+            headless: false, 
+            args: [`--window-size=1366,720`], 
+            defaultViewport: { width:1366, height: 720},
+            executablePath: "/usr/bin/google-chrome-stable",
+            userDataDir: '~/.config/google-chrome'
+        }
+    );
     const page = await browser.newPage();
     const now = new Date();
     console.log(now);
-    navigate(page,wait_for_rome=true);
+    navigate(page,wait_for_rome=false, login=true);
 })();
